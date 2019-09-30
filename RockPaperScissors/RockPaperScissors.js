3ee508f23738b58199490e80de3f6c3769610ac0
gkmiriyala <gkmiriyala@gmail.com>
2019-09-29 21:32:46 -0700

    Adding match tracking functionality

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -23,11 +23,38 @@ const RPSgame = () => {
         const computerHand = document.querySelector('computer-hand')
 
         //Computer Options
-        
+        const computerOptions = ['rock','paper','scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+            //Computer Choice
+                const computerNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[computerNumber];
+                //Here is where we call compare hands
+
+            });
+        })
+
     };
+
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update Text
+        const winner = document.querySelector('.winner')
+
+        //Checking for a tie
+        if (playerChoice == computerChoice) {
+            winner.textContent=='It is a tie';
+            return;
+        }
+
+        //Check for Rock
+        if(playerChoice)
+
+    }
 
     //call all the inner function
     startGame();
+    playMatch();
 };
 
 //start the game function
